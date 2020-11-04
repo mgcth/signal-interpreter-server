baf3387fad90f26106cfd5fea9506e64c7766001
Weng, Chenyang <chenyang.weng@volvocars.com>
2020-11-04 15:49:09 +0100

    add files

---
+++ main.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+DESCRIPTION
+
+Created by CWENG at 2020-11-04
+Current project: VeldiPython
+
+
+"""
+from routes import signal_interpreter_app
+
+signal_interpreter_app.run()
