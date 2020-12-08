c2e31b33611e0ad3fe82ea319517844fc49bed7f
Weng <CWENG@volvocars.com>
2020-12-08 14:56:19 +0100

    log files are ignored

--- signal_interpreter_server/exceptions.py
+++ signal_interpreter_server/exceptions.py
@@ -2,11 +2,17 @@
 Exceptions file.
 """
 
+class JsonParserError(Exception):
+    """
+    JsonParser General error
+    """
 
 class JsonParserLoadError(Exception):
     """
     JsonParser load_file error: File not found.
     """
+
+
 
 class JsonParserGetTitleError(Exception):
     """
